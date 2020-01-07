ddcfb9722fcb7784ca1b88a01e54b75a499a987a
Boris Korzun <b.korzun@gmail.com>
2020-01-07 20:36:20 +0300

    Change user in depoysh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-username=$(whoami)
+username=reddit
 home_dir=/home/$username
+
+adduser --system --disabled-password $username
+
 cd $home_dir
 git clone -b monolith https://github.com/express42/reddit.git
 cd $home_dir/reddit && bundle install
